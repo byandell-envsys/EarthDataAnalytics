1af4ea639e9936ee4175c630767596c9ef5377c1
byandell <byandell@wisc.edu>
2024-11-06 13:38:00 -0600

    update spiral R code

--- R/spiral.R
+++ R/spiral.R
@@ -1,12 +1,14 @@
 # https://allancameron.github.io/geomtextpath/
+library(ggplot2)
 
-t <- seq(5, -1, length.out = 1000) * pi
+n <- 1500
+t <- seq(5, -1, length.out = n) * pi
 
-spiral <- data.frame(x    = sin(t) * 1:1000, 
-                     y    = cos(t) * 1:1000,
-                     text = paste(rep.int("byandell Tribal", 9), collapse = " "))
+spiral <- data.frame(x    = sin(t) * 1:n, 
+                     y    = cos(t) * 1:n,
+                     text = paste(rep.int("byandell-envsys", 9), collapse = " "))
 
-ggplot(spiral, aes(x, y, label = text)) +
-  geomtextpath::geom_textpath(size = 7, vjust = 2, text_only = TRUE) +
+ggplot(spiral, aes(x, y, label = text, size = size)) +
+  geomtextpath::geom_textpath(size = 8, vjust = 2, text_only = TRUE) +
   coord_equal(xlim = c(-1500, 1500), ylim = c(-1500, 1500)) +
   theme_void()
